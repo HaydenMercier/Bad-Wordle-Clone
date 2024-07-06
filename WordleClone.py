373e4c2b1fd9e60401a2b6a9158e99d06c0d5df8
UltraArcher <ultra.archerhm@gmail.com>
2024-07-06 21:30:58 +1000

    hour 2

--- WordleClone.py
+++ WordleClone.py
@@ -1,7 +1,11 @@
 import random
 words = []
+lettersChecked = []
+lettersOut = []
+lettersIn = []
 guess = ""
 display = ""
+guesses = 0
 with open("output.txt") as file:
     for line in file:
         words.append(line.strip("\n"))
@@ -9,14 +13,26 @@ with open("output.txt") as file:
     print(word)
     while guess is not word:
         guess = input("Guess: ")
+        guesses +=1
         if guess == word:
-            print(f"Thats correct.\nThe word was {word}.")
+            print(f"That is correct.\nThe word was {word}.")
+            print(f"It took you {guesses} guesses!")
             break
         for i in guess:
-            if i not in word:
+            if word.index(i) == guess.index(i):
+                display += i
+            elif i not in word:
                 print(f"{i} is not in {word.capitalize()}.")
-                guess += "-"
-            if i in word:
+                display += "-"
+                lettersOut.append(i)
+                lettersChecked.append(i)
+            elif i in word:
                 print(f"{i} is in {word.capitalize()}.")
-                guess += "i"
-        
+                lettersIn.append(i)
+                lettersChecked.append(i)
+                display += "-"
+        print(display)
+        print(f"You have guessed: {', '.join(lettersChecked).upper()}.")
+        print(f"Incorrect guesses: {', '.join(lettersOut)}.")
+        print(f"Correct guesses: {','.join(lettersIn)}.")
+        print(f"Currently, the word is: {display}.")
